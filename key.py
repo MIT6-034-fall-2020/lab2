d3498a5d11eb26e47bc8e6ca7e8ae80054f93920
Nabib Ahmed <nahmed3536@gmail.com>
2020-10-09 14:21:47 -0400

    .gitignore

--- key.py
+++ key.py
@@ -1,6 +1,6 @@
 # Login information for the 6.034 Automated Tester
 
 USERNAME="nabibahm_MIT_EDU"
-PASSWORD="5uhXJj2YuXQ4NRZfAnLm"
+PASSWORD="GXTXVwhP64RvJyVRKYe2"
 XMLRPC_URL="https://ai6034.mit.edu/labs/xmlrpc/"
 
